47cf4e506899ece3f2125df650dcd4f770cfaca9
grin0021 <55245260+grin0021@users.noreply.github.com>
2025-03-17 11:06:35 -0400

    Updating project
    Started a graphics programming short course on Udemy. So far, the content has been about adding the necessary content to the CMakeLists file and initializing glfw/creating a window

---
+++ src/glfw_initialization.cpp
@@ -0,0 +1,16 @@
+#include <glfw_initialization.h>
+#include <cstdlib>
+#include <GLFW/glfw3.h>
+
+namespace veng {
+
+GlfwInitialization::GlfwInitialization() {
+  if (glfwInit() != GLFW_TRUE) {
+    std::exit(EXIT_FAILURE);
+  }
+}
+
+GlfwInitialization::~GlfwInitialization() {
+}
+
+}  // namespace veng
---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include <cstdint>
+#include <cmath>
+#include <gsl/gsl>
+#include "GLFW/glfw3.h"
+
+#include <glfw_initialization.h>
+
+std::int32_t main(std::int32_t argc, gsl::zstring* argv) {
+  veng::GlfwInitialization _glfw;
+
+  gsl::not_null<GLFWwindow*> window = glfwCreateWindow(800, 600, "Vulkan Engine", nullptr, nullptr);
+  gsl::final_action _cleanup_window([window]() {
+    glfwDestroyWindow(window);
+  });
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
+  return EXIT_SUCCESS;
+}
